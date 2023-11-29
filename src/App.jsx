bfa48a3ee7850646a2160804c62fae99e1c16b6a
Legui <leguigol@hotmail.com>
2023-11-29 16:49:05 -0300

    hasta el inicio del 93

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CrossIcon from './components/icons/CrossIcon'
+import MoonIcon from './components/icons/MoonIcon'
 
 const App=()=>{
   return (
@@ -6,7 +7,7 @@ const App=()=>{
       <header className="container mx-auto px-4 pt-8">
         <div className="flex justify-between">
           <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.5em]">Todo</h1>
-          <button>luna</button>
+          <button><MoonIcon className="fill-red-400"/></button>
         </div>
         <form className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4 mt-8">
           <span className="rounded-full border-2 w-5 h-5 inline-block"></span>
@@ -14,35 +15,36 @@ const App=()=>{
         </form>
       </header> 
       <main className="container mx-auto px-4 mt-8">
-        <div className="bg-white rounded-md px-4">
-          <article className='flex gap-4 py-4'>
-            <button className="rounded-full border-2 w-5 h-5 inline-block"></button>
-            <p className="text-gray-600">Complete online Javascript curse in Leguiweb</p>
-            <button className='ml-auto'><CrossIcon /></button>
+        <div className="bg-white rounded-md">
+          <article className='flex gap-4 py-4 border-b border-b-gray-400 px-4'>
+            <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+            <p className="text-gray-600 grow">Complete online Javascript curse in Leguiweb</p>
+            <button className='flex-none'><CrossIcon /></button>
           </article>
-          <article >
-            <button className="rounded-full border-2 w-5 h-5 inline-block"></button>
-            <p className="text-gray-600">Complete online Javascript curse in Leguiweb</p>
-            <button><CrossIcon /></button>
+          <article className='flex gap-4 py-4 border-b border-b-gray-400 px-4'>
+            <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+            <p className="text-gray-600 grow">Complete online Javascript curse in Leguiweb</p>
+            <button className='flex-none'><CrossIcon /></button>
           </article>
-          <article >
-            <button className="rounded-full border-2 w-5 h-5 inline-block"></button>
-            <p className="text-gray-600">Complete online Javascript curse in Leguiweb</p>
-            <button><CrossIcon /></button>
+          <article className='flex gap-4 py-4 border-b border-b-gray-400 px-4'>
+            <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+            <p className="text-gray-600 grow">Complete online Javascript curse in Leguiweb</p>
+            <button className='flex-none'><CrossIcon /></button>
           </article>
-          <section>
-            <span>5 items left</span>
-            <button>Clear Completed</button>
+          <section className='py-4 px-4 flex justify-between'>
+            <span className='text-gray-400'>5 items left</span>
+            <button className='text-gray-400'>Clear Completed</button>
           </section>
-
         </div>
 
       </main>
 
-      <section className="container mx-auto px-4">
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+      <section className="container mx-auto px-4 mt-8">
+        <div className='bg-white rounded flex justify-center gap-4'>
+          <button className='hover:text-blue-600'>All</button>
+          <button className='hover:text-blue-600'>Active</button>
+          <button className='hover:text-blue-600'>Completed</button>
+        </div>
       </section>
 
       <p className="text-center">Drag and drop to reorder list</p>
